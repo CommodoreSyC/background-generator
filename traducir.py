c394a3d6b2b5ee0f13a094bef8ef4a3a5f9a1eb0
Romero Ibon <Iromero@ikerlan.es>
2020-02-06 10:29:46 +0100

    adding starting project

---
+++ traducir.py
@@ -0,0 +1,15 @@
+
+
+from translate import Translator
+translator = Translator (to_lang = "ja")
+
+try:
+	with open('./texto.txt', mode = 'r') as my_file:
+
+		text = my_file.read()
+		translation = translator.translate(text)
+		print(translation
+			)
+except FileNotFoundError as e:
+	print('Check your file path silly!')
+
